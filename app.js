1388dc2246bffe7222ac9d85028bf0a33248e449
MoatazHammouda <moatazhammouda4@gmail.com>
2020-09-21 16:28:36 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+var noble = require("./noble");
+var counter = 0;
+
+noble.on('stateChange', function(state) {
+	if (state === 'poweredOn'){
+		noble.startScanning();
+	} else {
+		noble.stopScanning();
+	}
+});
+
+noble.on('discover', function(peripheral) {
+	//console.log(peripheral.rssi + ':' + peripheral.advertisement.localName)
+	if (peripheral.advertisement.localName === "MetaWear"){
+	console.log('Name: '+ peripheral.address); console.log('RSSI:' + peripheral.rssi);
+	count = count + 1;
+	if (count==2){
+	process.exit(10);}}
+});
